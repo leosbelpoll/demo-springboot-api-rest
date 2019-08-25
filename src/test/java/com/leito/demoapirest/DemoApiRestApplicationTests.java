276251dbed7334a26bc6b07666fe5e9c6438603a
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2019-08-24 23:19:44 -0300

    First test (fake)

--- src/test/java/com/leito/demoapirest/DemoApiRestApplicationTests.java
+++ src/test/java/com/leito/demoapirest/DemoApiRestApplicationTests.java
@@ -1,13 +1,15 @@
 package com.leito.demoapirest;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApiRestApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void unit_fake_test() {
+		Assertions.assertSame("fakeTest", "fakeTest");
 	}
 
 }
